cff0b7cf2125e1628e7c08e3b3f06f93167f323b
wang645@126.com <19810201>
2020-06-16 20:29:51 +0800

    init commit

---
+++ src/main/java/com/xtxb/cmdb/CmdbApplication.java
@@ -0,0 +1,13 @@
+package com.xtxb.cmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmdbApplication.class, args);
+    }
+
+}
