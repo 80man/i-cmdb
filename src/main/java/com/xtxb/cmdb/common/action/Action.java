3759b5e89255c4f66c9c9364b30898d65be02480
wang645@126.com <19810201>
2020-06-17 16:45:58 +0800

    更新java doc

--- src/main/java/com/xtxb/cmdb/common/action/Action.java
+++ src/main/java/com/xtxb/cmdb/common/action/Action.java
@@ -1,13 +1,12 @@
 package com.xtxb.cmdb.common.action;
 
 /**
- * 作者: xtxb-fedora
+ * 作者: xtxb
  * <p>
  * 日期: 2020年06月17日-下午3:33
  * <p>
- * 版权说明：北京神州泰岳软件股份有限公司
  * <p>
- * TODO
+ *
  */
 public interface Action {
     public String getID();
