31c6bfbbd9fb20363520fd9ce5bb9b0acd574373
wang645@126.com <19810201>
2020-07-21 15:40:23 +0800

    删除模型继承，采用视图进行组织

--- init/DDL.sql
+++ init/DDL.sql
@@ -1,7 +1,7 @@
 CREATE TABLE M_META (
 ENNAME varchar(32),
-CNNAME varchar(32),
-PNAME varchar(32));
+CNNAME varchar(32)
+);
  CREATE TABLE P_META (
 ENNAME varchar(32),
 CNNAME varchar(100),
@@ -21,47 +21,31 @@ TARGETMODEL varchar(32)
 CREATE TABLE C_FIREWALL (
 P_OID numeric(20)  not null primary key,
 P_SID varchar(32) ,
-P_cpubit  numeric(20),
-P_ip  varchar(200),
-P_weight  numeric(20,2),
-P_power  numeric(20)
+P_IP  varchar(200)
 );
 CREATE INDEX C_FIREWALL_IND_P_SID ON C_FIREWALL (P_SID);
-
-CREATE TABLE C_ITDEVICE (
-P_OID numeric(20)  not null primary key,
-P_SID varchar(32) ,
-P_cpubit  numeric(20),
-P_ip  varchar(200),
-P_weight  numeric(20,2),
-P_power  numeric(20)
-);
-CREATE INDEX C_ITDEVICE_IND_P_SID ON C_ITDEVICE (P_SID);
 
 CREATE TABLE C_HOST (
 P_OID numeric(20)  not null primary key,
 P_SID varchar(32) ,
-P_cpubit  numeric(20),
-P_ip  varchar(200),
-P_weight  numeric(20,2),
-P_power  numeric(20)
+P_CPUBIT  numeric(20),
+P_IP  varchar(200),
+P_POWER  numeric(20),
+P_WEIGHT  numeric(20,2)
 );
 CREATE INDEX C_HOST_IND_P_SID ON C_HOST (P_SID);
 
 CREATE TABLE C_SWITCH (
 P_OID numeric(20)  not null primary key,
 P_SID varchar(32) ,
-P_cpubit  numeric(20),
-P_ip  varchar(200),
-P_weight  numeric(20,2),
-P_power  numeric(20)
+P_IP  varchar(200)
 );
 CREATE INDEX C_SWITCH_IND_P_SID ON C_SWITCH (P_SID);
 
-CREATE TABLE R_Connection (
+CREATE TABLE R_CONNECTION (
 R_SID numeric(20),
 R_TID numeric(20),
 R_NOTE varchar(100)
 );
-CREATE INDEX R_Connection_IND ON R_Connection (R_SID,R_TID);
+CREATE INDEX R_CONNECTION_IND ON R_CONNECTION (R_SID,R_TID);
 
--- init/DML.sql
+++ init/DML.sql
@@ -1,11 +1,12 @@
-insert into  M_META values('C_FireWall','防火墙','C_ITDevice' );
-insert into  M_META values('C_ITDevice','IT设备',NULL );
-insert into  M_META values('C_Host','服务器','C_ITDevice' );
-insert into  M_META values('C_Switch','交换机','C_ITDevice' );
+insert into  M_META values('C_FireWall','防火墙' );
+insert into  M_META values('C_Host','服务器' );
+insert into  M_META values('C_Switch','交换机' );
 
-insert into  P_META values('cpubit','CPU位数','C_ITDevice','基本信息',2,'64',1,'32|64');
-insert into  P_META values('ip','管理IP','C_ITDevice','基本信息',1,NULL,0,NULL);
-insert into  P_META values('weight','重量(kg)','C_ITDevice','基本信息',3,NULL,0,NULL);
-insert into  P_META values('power','电源功率(W)','C_ITDevice','基本信息',2,NULL,0,NULL);
+insert into  P_META values('cpubit','CPU位数','C_Host','基本信息',2,'64',1,'32|64');
+insert into  P_META values('ip','管理IP','C_Switch','基本信息',1,NULL,0,NULL);
+insert into  P_META values('ip','管理IP','C_Host','基本信息',1,NULL,0,NULL);
+insert into  P_META values('power','电源功率(W)','C_Host','基本信息',2,NULL,0,NULL);
+insert into  P_META values('ip','管理IP','C_FireWall','基本信息',1,NULL,0,NULL);
+insert into  P_META values('weight','重量(kg)','C_Host','基本信息',3,NULL,0,NULL);
 
-insert into  R_META values('R_Connection','网络连接','服务器','交换机');
+insert into  R_META values('R_Connection','网络连接','C_Host','C_Switch');
